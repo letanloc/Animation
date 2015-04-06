dd7d072ef0605ba32d53815fb174e132780ce122
letanloc <anhyeuemlathat296@gmail.com>
2015-04-06 18:03:45 +0700

    Initial commit

---
+++ mylibrary/src/main/java/com/example/letanloc/mylibrary/RssFeedProvide.java
@@ -0,0 +1,23 @@
+package com.example.letanloc.mylibrary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by letanloc on 06/04/2015.
+ */
+public class RssFeedProvide {
+
+    public static List<RssItem> listparse(String rssFeed) {
+//        RssIte  m Item = new RssItem("Test1", "tes2");
+//        list.add(item);
+//
+        List<RssItem> list = new ArrayList<>();
+        RssItem item = new RssItem("s", "y");
+        list.add(item);
+
+        return list;
+    }
+
+
+}
